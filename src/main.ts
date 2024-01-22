f3537b2d1673cfac702b0b1a6194643cbe4be7f6
Delemangi <milev.stefan@gmail.com>
2024-01-22 23:16:27 +0100

    Move source file

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import { existsSync } from 'node:fs';
-import {
-  mkdir,
-  writeFile
-} from 'node:fs/promises';
-import { argv } from 'node:process';
+import { mkdir, writeFile } from 'node:fs/promises';
 import { launch } from 'puppeteer';
-import {
-  createLogger,
-  format,
-  transports
-} from 'winston';
+import { createLogger, format, transports } from 'winston';
 
-const sessionSelector = 'a > img[src^="https://courses.finki.ukim.mk/theme/image.php/classic/bigbluebuttonbn/"]';
+const sessionSelector =
+  'a > img[src^="https://courses.finki.ukim.mk/theme/image.php/classic/bigbluebuttonbn/"]';
 const tableSelector = 'table.generaltable';
 const titleSelector = 'div[role="main"] > h3';
 const courseSelector = 'ol.breadcrumb > li:nth-child(3) > a';
@@ -29,22 +22,21 @@ const logger = createLogger({
             info: 'green',
             silly: 'magenta',
             verbose: 'cyan',
-            warn: 'yellow'
-          }
+            warn: 'yellow',
+          },
         }),
-        format.printf(({
-          level,
-          message,
-          timestamp
-        }) => `${timestamp} - ${level}: ${message}`)
+        format.printf(
+          ({ level, message, timestamp }) =>
+            `${timestamp} - ${level}: ${message}`,
+        ),
       ),
       handleExceptions: true,
-      level: 'info'
-    })
-  ]
+      level: 'info',
+    }),
+  ],
 });
 
-const [cookie, url] = argv.slice(2);
+const [cookie, url] = process.argv.slice(2);
 
 if (cookie === undefined || url === undefined) {
   throw new Error('Missing cookie or url');
@@ -53,7 +45,7 @@ if (cookie === undefined || url === undefined) {
 const cookies = {
   domain: 'courses.finki.ukim.mk',
   name: 'MoodleSession',
-  value: cookie
+  value: cookie,
 };
 
 const browser = await launch({ headless: false });
@@ -62,21 +54,32 @@ const page = await browser.newPage();
 await page.setCookie(cookies);
 await page.goto(url);
 
-const sessions = await page.$$eval(sessionSelector, (elements) => elements.map((e) => e.parentElement?.getAttribute('href')));
-const course = await page.$eval(courseSelector, (e) => e.title);
+const sessions = await page.$$eval(
+  sessionSelector,
+  (elements) =>
+    elements
+      .map((element) => element.parentElement?.getAttribute('href'))
+      .filter(Boolean) as string[],
+);
+const course = await page.$eval(courseSelector, (element) => element.title);
 const log: string[] = [];
 
 for (const session of sessions) {
-  if (session === undefined || session === null) {
-    continue;
-  }
-
   await page.goto(session);
 
-  const title = await page.$eval(titleSelector, (e) => e.textContent);
-  let links;
+  const title = await page.$eval(
+    titleSelector,
+    (element) => element.textContent,
+  );
+  let links: string[];
   try {
-    links = await page.$eval(tableSelector, (e) => Array.from(e.querySelectorAll('a')).map((el) => el.dataset['href']));
+    links = await page.$eval(
+      tableSelector,
+      (table) =>
+        Array.from(table.querySelectorAll('a'))
+          .map((element) => element.dataset['href'])
+          .filter(Boolean) as string[],
+    );
   } catch {
     logger.info(`${title} has no recordings`);
     continue;
@@ -86,7 +89,6 @@ for (const session of sessions) {
 
   for (const link of links) {
     if (link === undefined || link === null) {
-      logger.warn('e');
       continue;
     }
 
@@ -102,4 +104,8 @@ if (!existsSync('output')) {
   await mkdir('output');
 }
 
-await writeFile(`output/${course.replaceAll(/[\\/]/gu, '')}.txt`, log.join('\n'), { flag: 'w' });
+await writeFile(
+  `output/${course.replaceAll(/[\\/]/gu, '')}.txt`,
+  log.join('\n'),
+  { flag: 'w' },
+);
